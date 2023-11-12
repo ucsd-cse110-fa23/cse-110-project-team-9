184479bd599a44736920793a11b3321147f47a2d
dancarrascoza <71916878+danielcarrascoza@users.noreply.github.com>
2023-11-12 02:01:14 -0800

    Create AppFrameTest.java

---
+++ src/test/java/client/AppFrameTest.java
@@ -0,0 +1,13 @@
+package client;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AppFrameTest {
+    
+}
