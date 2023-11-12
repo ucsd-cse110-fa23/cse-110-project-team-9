3f7ed62d53d98867547efde4d9189817e66048f4
pranavMekkoth1 <pmekkoth@ucsd.edu>
2023-11-12 12:52:51 -0800

    updated code with gradle included

--- src/main/java/client/AppFrame.java
+++ src/main/java/client/AppFrame.java
@@ -30,8 +30,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.text.TextAlignment;
 import javafx.geometry.Insets;
 import javafx.scene.text.*;
 import java.io.*;
@@ -45,8 +43,10 @@ public class AppFrame extends Stage {
     private AudioFormat audioFormat;
     private TargetDataLine targetDataLine;
     private Label recordingLabel;
-    //private RecipeController recipeController;
+    // private RecipeController recipeController;
     private RecipeList recipeList;
+    private String currRecipeText;
+    private String currRecipeType;
 
     Button addButton;
 
@@ -54,7 +54,7 @@ public class AppFrame extends Stage {
         setTitle("Recipe List View");
 
         BorderPane mainLayout = createMainLayout();
-        Scene mainScene = new Scene(mainLayout, 800, 600);
+        Scene mainScene = new Scene(mainLayout, 600, 600);
         audioFormat = getAudioFormat();
         setScene(mainScene);
 
@@ -78,102 +78,123 @@ public class AppFrame extends Stage {
     }
 
     private void openPopup() {
+
+        Recipe recipe = new Recipe();
+
+        // currRecipeText = "Current Recipe: ";
+        // currRecipeType = "Recipe Type: ";
+
+        Label recipeText = new Label();
+        Label recipeType = new Label();
+
+        recipeText.setVisible(false);
+        recipeType.setVisible(false);
+
         Stage popupStage = new Stage();
         popupStage.initModality(Modality.APPLICATION_MODAL);
         popupStage.initOwner(this); // Set the main stage as the owner
         popupStage.setTitle("Creating Recipe");
 
-        Label recordingLabel = new Label("Recording. . .");
-        recordingLabel.setVisible(false);
+        Label recordingLabel1 = new Label("Recording. . .");
+        Label recordingLabel2 = new Label("Recording. . .");
+        recordingLabel1.setVisible(false);
+        recordingLabel2.setVisible(false);
 
-        Button closeButton = new Button("Cancel");
-        closeButton.setOnAction(e -> popupStage.close());
+        Button recTypeStart = new Button("Start Recording Meal Type (Breakfast, Lunch, Dinner)");
+        recTypeStart.setOnAction(e -> {
+            startRecording();
+            recordingLabel1.setVisible(true);
+        });
 
-        BorderPane popupLayout = new BorderPane();
-        popupLayout.setBottom(closeButton);
+        Button recTypeStop = new Button("Stop Recording Meal Type");
+        recTypeStop.setOnAction(e -> {
+            recordingLabel1.setVisible(false);
+            stopRecording();
+            audioToText();
+            setType();
+            String typeLabel = "Recipe Type: " + currRecipeType;
+            recipeType.setText(typeLabel);
+            recipeType.setVisible(true);
+        });
 
-        Button startRecording = new Button("start recording ingredients");
+        Button startRecording = new Button("Start Recording Ingredients");
         startRecording.setOnAction(e -> {
             startRecording();
-            recordingLabel.setVisible(true);
+            recordingLabel2.setVisible(true);
         });
-        Button stopRecording = new Button("stop recording ingredients");
-        stopRecording.setOnAction(e1 -> {
-            recordingLabel.setVisible(false);
-            popupStage.close();
+        Button stopRecording = new Button("Stop Recording Ingredients");
+        stopRecording.setOnAction(e -> {
+            recordingLabel2.setVisible(false);
             stopRecording();
-            Recipe recipe = new Recipe();
-            recipeList.getChildren().add(recipe);
             audioToText();
             ingredientsToRecipe();
-            recipe.setRecipe(ChatGPT.returnPrompt());
-            //recipe.setRecipe("eat");
-            popupStage.close();
-            recipe.getDeleteButton().setOnAction(e3->{
-                recipeList.getChildren().remove(recipe);
-        });
+            currRecipeText = ChatGPT.getResult(); 
+            String recipeLabel = "Recipe Preview: " + currRecipeText;
+            recipeText.setText(recipeLabel);
+            recipeText.setVisible(true);
+
+            // recipe.setRecipe("eat");
         });
 
-        Scene popupScene = new Scene(popupLayout, 400, 300);
+        Button closeButton = new Button("Cancel Recipe");
+        closeButton.setOnAction(e -> popupStage.close());
+
+        recipe.getDeleteButton().setOnAction(e -> {
+            recipeList.deleteRecipe(recipe);
+        });
+
+        Button saveRecipe = new Button("Save Recipe");
+        saveRecipe.setOnAction(e -> {
+            recipe.setRecipeName(ChatGPT.returnPrompt());
+            recipe.setRecipeTotal(currRecipeText);
+            recipeList.getChildren().add(recipe);
+            popupStage.close();
+
+        });
+
+        BorderPane popupLayout = new BorderPane();
+        Scene popupScene = new Scene(popupLayout, 600, 600);
         popupStage.setScene(popupScene);
 
-        HBox buttonBox = new HBox(10);
-        buttonBox.getChildren().addAll(startRecording, stopRecording, recordingLabel);
+        HBox typeBox = new HBox();
+        typeBox.getChildren().addAll(recTypeStart, recTypeStop, recordingLabel1);
+        typeBox.setAlignment(Pos.CENTER);
+        typeBox.setSpacing(5);
 
-        popupLayout.setCenter(buttonBox);
+        HBox recordingBox = new HBox();
+        recordingBox.getChildren().addAll(startRecording, stopRecording, recordingLabel2);
+        recordingBox.setSpacing(5);
+        recordingBox.setAlignment(Pos.CENTER);
+
+        VBox topBox = new VBox();
+        topBox.getChildren().addAll(typeBox, recordingBox);
+        topBox.setSpacing(10);
+
+        popupLayout.setTop(topBox);
+
+        VBox middleBox = new VBox();
+
+        String typeLabel = "Recipe Type: " + currRecipeType;
+        String recipeLabel = "Recipe Preview: " + currRecipeText;
+        recipeType.setText(typeLabel);
+        recipeText.setText(recipeLabel);
+        middleBox.getChildren().addAll(recipeType, recipeText);
+        middleBox.setSpacing(5);
+
+        ScrollPane scroller = new ScrollPane(middleBox);
+        scroller.setFitToWidth(true);
+        scroller.setFitToHeight(true);
+        popupLayout.setCenter(scroller);
+
+        HBox bottomBox = new HBox();
+        bottomBox.getChildren().addAll(closeButton, saveRecipe);
+        bottomBox.setAlignment(Pos.CENTER);
+        bottomBox.setSpacing(5);
+        popupLayout.setBottom(bottomBox);
 
         popupStage.showAndWait();
     }
-    
-    
 
-    class Footer extends HBox {
-
-        private Button addButton;
-    
-        Footer() {
-            this.setPrefSize(500, 60);
-            this.setStyle("-fx-background-color: #F0F8FF;");
-            this.setSpacing(15);
-    
-            // set a default style for buttons - background color, font size, italics
-            String defaultButtonStyle = "-fx-font-style: italic; -fx-background-color: #FFFFFF;  -fx-font-weight: bold; -fx-font: 11 arial;";
-    
-            addButton = new Button("Create a Recipe"); // text displayed on add button
-            addButton.setStyle(defaultButtonStyle); // styling the button
-
-            addButton.setOnAction(e -> {
-                openPopup();
-            });
-    
-            this.getChildren().addAll(addButton);
-            this.setAlignment(Pos.CENTER); // aligning the buttons to center
-        }
-    
-        public Button getAddButton() {
-            return addButton;
-        }
-    }
-    class Header extends HBox {
-
-        Header() {
-        this.setPrefSize(500, 60); // Size of the header
-        this.setStyle("-fx-background-color: #F0F8FF;");
-
-        Text titleText = new Text("PantryPal"); // Text of the Header
-        titleText.setStyle("-fx-font-weight: bold; -fx-font-size: 20;");
-        this.getChildren().add(titleText);
-        this.setAlignment(Pos.CENTER); // Align the text to the Center
-        }
-    }
-    class RecipeList extends VBox {
-
-        RecipeList() {
-            this.setSpacing(3); // sets spacing between recipes
-            this.setPrefSize(500, 560);
-            this.setStyle("-fx-background-color: #F0F8FF;");
-        }
-    }
     private void audioToText() {
         String[] args = new String[] { "recording.wav" };
         try {
@@ -184,9 +205,9 @@ public class AppFrame extends Stage {
             f.printStackTrace();
         }
     }
-    
+
     private void ingredientsToRecipe() {
-        String[] args = new String[] { Whisper.getResult() };
+        String[] args = new String[] { Whisper.getResult(), currRecipeType };
         try {
             ChatGPT.main(args);
         } catch (IOException e) {
@@ -197,24 +218,29 @@ public class AppFrame extends Stage {
             e.printStackTrace();
         }
     }
-    
+
+    private void setType() {
+        String[] args = new String[] { Whisper.getResult() };
+        currRecipeType = args[0];
+    }
+
     private AudioFormat getAudioFormat() {
         // the number of samples of audio per second.
         // 44100 represents the typical sample rate for CD-quality audio.
         float sampleRate = 44100;
-    
+
         // the number of bits in each sample of a sound that has been digitized.
         int sampleSizeInBits = 16;
-    
+
         // the number of audio channels in this format (1 for mono, 2 for stereo).
         int channels = 2;
-    
+
         // whether the data is signed or unsigned.
         boolean signed = true;
-    
+
         // whether the audio data is stored in big-endian or little-endian order.
         boolean bigEndian = false;
-    
+
         return new AudioFormat(
                 sampleRate,
                 sampleSizeInBits,
@@ -222,13 +248,13 @@ public class AppFrame extends Stage {
                 signed,
                 bigEndian);
     }
-    
+
     private void startRecording() {
         Thread t = new Thread(
                 new Runnable() {
                     @Override
                     public void run() {
-    
+
                         try {
                             // the format of the TargetDataLine
                             DataLine.Info dataLineInfo = new DataLine.Info(
@@ -238,11 +264,11 @@ public class AppFrame extends Stage {
                             targetDataLine = (TargetDataLine) AudioSystem.getLine(dataLineInfo);
                             targetDataLine.open(audioFormat);
                             targetDataLine.start();
-    
+
                             // the AudioInputStream that will be used to write the audio data to a file
                             AudioInputStream audioInputStream = new AudioInputStream(
                                     targetDataLine);
-    
+
                             // the file that will contain the audio data
                             File audioFile = new File("recording.wav");
                             AudioSystem.write(
@@ -257,12 +283,67 @@ public class AppFrame extends Stage {
         t.start();
     }
 
-    //public Label getRecordingLable() {
-    //    return recordingLabel;
-  //  }
+    public Label getRecordingLable() {
+        return recordingLabel;
+    }
 
     private void stopRecording() {
         targetDataLine.stop();
         targetDataLine.close();
     }
+
+    class Footer extends HBox {
+
+        private Button addButton;
+
+        Footer() {
+            this.setPrefSize(500, 60);
+            this.setStyle("-fx-background-color: #553D67;");
+            this.setSpacing(15);
+
+            // set a default style for buttons - background color, font size, italics
+            String defaultButtonStyle = "-fx-font-style: italic; -fx-background-color: #F64C72;  -fx-font-weight: bold; -fx-font: 16 arial;";
+
+            addButton = new Button("Create a Recipe"); // text displayed on add button
+            addButton.setStyle(defaultButtonStyle); // styling the button
+
+            addButton.setOnAction(e -> {
+                openPopup();
+            });
+
+            this.getChildren().addAll(addButton);
+            this.setAlignment(Pos.CENTER); // aligning the buttons to center
+        }
+
+        public Button getAddButton() {
+            return addButton;
+        }
+    }
+
+    class Header extends HBox {
+
+        Header() {
+            this.setPrefSize(500, 60); // Size of the header
+            this.setStyle("-fx-background-color: #553D67;");
+
+            Text titleText = new Text("PantryPal"); // Text of the Header
+            titleText.setStyle("-fx-font-weight: bold; -fx-font-size: 20");
+            this.getChildren().add(titleText);
+            this.setAlignment(Pos.CENTER); // Align the text to the Center
+        }
+    }
+
+    class RecipeList extends VBox {
+
+        RecipeList() {
+            this.setSpacing(3); // sets spacing between recipes
+            this.setPrefSize(500, 560);
+            this.setStyle("-fx-background-color: #242582;");
+        }
+
+        public void deleteRecipe(Recipe recipe) {
+            this.getChildren().remove(recipe);
+        }
+    }
+
 }
